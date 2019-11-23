4a3b1e745475c1e5f0cdb80618a539c19e93ff9d
Julia Ellboeck <juliaellboeck@gmail.com>
2019-11-23 11:29:09 -0500

    Updated js

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -6,44 +6,49 @@ var limit;
 var parkCode;
 
 $("#submitBtn").on("click", function () {
+    event.preventDefault();
     state = $("#inputState").val();
-    //create loading gif and push onto page
-    var loader = $("<div>");
-    loader.attr("id", "loader");
-    loader.html("<h3>Get excited parks are on the way!</h3>")
-    loader.append("<img src=https://giphy.com/gifs/5tfqO7ClftEsoa6J7h>");
-    $("#loader").html(loader);
+    console.log(state);
+    // create loading gif and push onto page
+    // var loader = $("<div>");
+    // loader.attr("id", "loader");
+    // loader.html("<h3>Get excited parks are on the way!</h3>")
+    // loader.append("<img src=https://giphy.com/gifs/5tfqO7ClftEsoa6J7h>");
+    // $("#loader").html(loader);
 
-    //call functions
+    // call functions
     getParks()
 })
 
-$(document).ajaxStart(function () {
-    // Show image container
-    $("#row1").hide();
-    $("#row2").hide();
-    $("#loader").show();
-    setTimeout(function () {
-        $("#loader").hide();
-        $("#row1").show();
-        $("#row2").show();
-    }, 3000);
-});
+// $(document).ajaxStart(function () {
+//     // Show image container
+//     $("#row1").hide();
+//     $("#row2").hide();
+//     $("#loader").show();
+//     setTimeout(function () {
+//         $("#loader").hide();
+//         $("#row1").show();
+//         $("#row2").show();
+//     }, 3000);
+// });
 
 
 function getParks(){
-    var queryURLParks= "https://developer.nps.gov/api/v1/parks?&stateCode="+ state +"&limit="+ limit + "&api_key=" + parkAPIKey;
+    var queryURLParks= "https://developer.nps.gov/api/v1/parks?&stateCode="+ state +"&limit=10&api_key=" + parkAPIKey;
+    console.log(queryURLParks)
     
     $.ajax({
         url:queryURLParks,
         method: "GET"
     }).then(function (response){
-        parkCode= response.parkCode;
-        var cityName= response.name;
-        var url = response.url;
-        var description = response.description;
-        var directionsURL = response.directionsURL;
-        console.log(cityName, url, description);
+        console.log(response);
+        parkCode= response.data.parkCode;
+        var cityName= response.data.name;
+        var url = response.data.url;
+        var description = response.data.description;
+        var directionsURL = response.data.directionsURL;
+        console.log(cityName);
+        console.log(parkCode);
     })
 
 }
