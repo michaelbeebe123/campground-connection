fa3053405e6e100eec0ef433ac0e5118754d0fb7
Michael Beebe <michaelbeebe@Michaels-MacBook-Pro.local>
2019-11-26 18:44:38 -0500

    integrating the michael.js file into index.js

--- app/public/js/michael.js
+++ app/public/js/michael.js
@@ -5,7 +5,7 @@
 // VAR THAT CONTAINS html THAT WILL BE RENDERED ON THE FLY
 var parkCards = 
 `<div class="card mb-3">
-    <img src="..." class="card-img-top" alt="...">
+    <img src="${parkResults[i].parkPic}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title" id="park-name">${parkResults[i].name}</h5>
             <p class="card-text" id="park-description">${parkResults[i].description}</p>
@@ -16,5 +16,7 @@ var parkCards =
 
 // TODO: DUMPS THE PARK CARDS WHEN USER CLICKS SUBMIT BUTTON
 $("#submitBtn").on("click", function(){
-        $("#resultcards").html(parkCards);
+        for (var i =0; i < parkResults.length; i++) {
+            $("#resultcards").html(parkCards);
+        }
 })
