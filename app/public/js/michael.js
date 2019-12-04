997242471b2db81a2153e997425eb4620e7a6e94
Michael Beebe <michaelbeebe@Michaels-MacBook-Pro.local>
2019-12-03 19:33:48 -0500

    dynamic html is in michael.js for the campground pages

--- app/public/js/michael.js
+++ app/public/js/michael.js
@@ -1,23 +1,41 @@
-
-// TODO: ADD VALUES FROM parkResults ARRAY TO THE DYNAMIC HTML
-// TODO: ADD PARK IMAGE TO THE AJAX CALL
-
-// VAR THAT CONTAINS html THAT WILL BE RENDERED ON THE FLY
-for (var i = 0; i < parkResults.length; i++) {
-    var parkCards =
-        `<div class="card mb-3">
-            <div class="card-body">
-                <h5 class="card-title" id="park-name">${parkResults[i].parkName}</h5>
-                <p class="card-text" id="park-description">${parkResults[i].description}</p>
-                <p class ="card-text" id ="park-url" <a href="${parkResults[i].url}">Website</a>></p>
-                <p class ="card-text" id ="directions-url" <a href="${parkResults[i].directionsURL}">Directions</a>></p>
-                <button id="view-campgrounds">View Campgrounds</a></button>
-            </div>
-        </div>`
-
-    $("#resultcards").append(parkCards);
-
-    if (parkResults[i] === undefined) {
-        
-    }
-}
+var campDetails =
+    `<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">${newCampObject.parkName}</h5>
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+        <ul>
+          <li>Description: ${newCampObject.description}</li>
+          <li>URL: ${newCampObject.url}</li>
+          <li>Weather Overview: ${newCampObject.weatherOverview}</li>
+          <li>Directions Overview: ${newCampObject.directionsOverview}</li>
+          <h5>Amenities</h5>
+            <ul>
+                <li>Dump Station: ${newCampObject.dumpStation}</li>
+                <li>Cell-Phone Reception: ${newCampObject.cellPhone}</li>
+                <li>Toilets: ${newCampObject.toilets}</li>
+                <li>Showers: ${newCampObject.showers}</li>
+                <li>Internet Connectivity: ${newCampObject.internet}</li>
+                <li>Storage: ${newCampObject.storageLocker}</li>
+                <li>Camp Store: ${newCampObject.campStore}</li>
+                <li>Laundry: ${newCampObject.laundry}</li>
+                <li>Fire Stove: ${newCampObject.fireStove}</li>
+            </ul>
+          <h5>Regulations</h5>
+            <ul>
+                <li>Overview: ${newCampObject.regulationsOverview}</li>
+                <li>URL: ${newCampObject.regulationsURL}</li>
+            </ul>
+        </ul>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Go Back</button>
+          <button type="button" class="btn btn-primary">Add</button>
+        </div>
+      </div>
+    </div>
+  </div>`
